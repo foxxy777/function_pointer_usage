95f6a7c5d1e5214b58247fc08e4b401618af6505
foxxy777 <38045414+foxxy777@users.noreply.github.com>
2020-10-17 20:02:45 +0800

    Update main.cpp
    挂vpn不能sync，不挂又连不上外网，vs的git没法用了，只能手动在这更新

--- main.cpp
+++ main.cpp
@@ -8,13 +8,23 @@ int compare(int a, int b)
         return 0;
 }
 
-void bubblesort(int a[], int num_of_element, int (*compare)(int, int))
+int abs_compare(int a, int b)
 {
+    if (abs(a) > abs(b))
+        return 1;
+    else
+        return 0;
+}
+
+
+    //void bubblesort(int a[], int num_of_element),最普通地调用函数，不用专门指出指针，直接在函数里面调用就可以了
+void bubblesort(int a[], int num_of_element, int (*call_back_compare)(int, int))//使用函数指针的好处是，可以在这里通过参数的形式来调用不同的处理函数，可以在这选择我们要的是compare还是abs_compare
+{//注意，这里的call_back_compare只是形参名字而已，我们只要保证传进去的call_back函数参数和返回值是这里规定的格式就可以了，从而实现了可以丢不同的callback函数进去
     int temp;
     for(int i = 0;i<num_of_element;i++)
         for (int j = 0; j < num_of_element - 1; j++)
         {   
-            if (compare(a[j], a[j + 1]) == 1)
+            if (call_back_compare(a[j], a[j + 1]) == 1)
             {
                 temp = a[j];
                 a[j] = a[j + 1];
@@ -23,11 +33,17 @@ void bubblesort(int a[], int num_of_element, int (*compare)(int, int))
         }
 }
 
+
+//check git change
+
+
 int main()
 {
 
     int a[] = {2,4,6,8,9};
-    bubblesort(a, 5,compare);
+    //bubblesort(a, 5);
+    //bubblesort(a, 5,compare);
+    bubblesort(a, 5,abs_compare);
     for (int i = 0; i < 5; i++)
     {
         cout << a[i] << endl;
